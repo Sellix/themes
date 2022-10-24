84a23b4e0b4dc05fdaf6996d8c98967ee6735da9
Daniele Servadei <servadei.d@gmail.com>
2022-10-24 18:43:08 +0200

    add scripts to zip theme

---
+++ scripts/zip.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+die () {
+  echo >&2 "$@"
+  exit 1
+}
+
+[ "$#" -eq 2 ] || die "usage: zip.sh themeName outputVersion"
+
+echo $1 | grep -E -q '^[a-zA-Z0-9_.-]*$' || die "String argument required, $1 provided; not matchin regex"
+echo $2 | grep -E -q '^[a-zA-Z0-9_.-]*$' || die "String argument required, $2 provided; not matchin regex"
+
+[ -d "$1" ] || die "Theme $1 does not exist"
+
+zip -r ./output/$2.zip $1/*
