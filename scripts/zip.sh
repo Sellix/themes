bf0e29fc35ccf284151fb0d4da209ad4f9a4ee1c
iRaxe <djvinx@live.com>
2022-12-07 17:34:49 +0100

    Revert "Merge branch 'main' of https://github.com/Sellix/themes"
    This reverts commit a78ff516fb688f98c001ac1d99c121c38c0128c1, reversing
    changes made to e884877fb64e9579be4330c991b397fa8ea32063.

--- scripts/zip.sh
+++ scripts/zip.sh
@@ -10,8 +10,8 @@ die () {
 echo $1 | grep -E -q '^[a-zA-Z0-9_.-]*$' || die "String argument required, $1 provided; not matching regex"
 echo $2 | grep -E -q '^[a-zA-Z0-9_.-]*$' || die "String argument required, $2 provided; not matching regex"
 
-[ -d "themes/$1" ] || die "Theme $1 does not exist"
+[ -d "$1" ] || die "Theme $1 does not exist"
 
 cd themes/$1
-zip -r ../../output/$2.zip *
+zip -r ../output/$2.zip *
 cd ../..
