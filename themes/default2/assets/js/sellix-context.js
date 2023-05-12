152ccbea478de4cbb2f338d1c46759785e54be77
Dzmitry <dzmitry.y@5ly.co>
2023-05-12 13:09:27 +0200

    Added full products list into Render Context

--- themes/default2/assets/js/sellix-context.js
+++ themes/default2/assets/js/sellix-context.js
@@ -7,6 +7,25 @@
     static set(name, value) {
       context[name] = value;
     }
+
+    static getShopInfo() {
+      return this.get('common', {}).shopInfo || {};
+    }
+
+    static getShopItems() {
+      const products = this.getShopProducts();
+      return (this.getShopInfo().items || [])
+        .filter((itemId) => Boolean(products[itemId]))
+        .map((itemId) => products[itemId]);
+    }
+
+    static getShopProducts() {
+      return this.getShopInfo().products || {};
+    }
+
+    static getShopCategories() {
+      return this.getShopInfo().categories || {};
+    }
   }
 
   window.SellixContext = Context;
