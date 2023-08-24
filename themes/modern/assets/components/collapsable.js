cf5abd98216ff1d246bc222e3fead256c8445ece
Dzmitry <dzmitry.y@5ly.co>
2023-08-25 00:05:46 +0200

    - v1.0.58 - Issue: Collapse product's Description, Terms and Warranty not good - Issue: Tooltip - Issue: Reviews to big gap from top

--- themes/modern/assets/components/collapsable.js
+++ themes/modern/assets/components/collapsable.js
@@ -1,15 +1,46 @@
 (function (document, window, jQuery) {
   class CollapsableComponent {
-    constructor(headerSelector, bodySelector) {
+    constructor(headerSelector, bodySelector, options = {}) {
       this.opened = false;
       this.$header = jQuery(headerSelector);
       this.$body = jQuery(bodySelector);
       this.$header.on('click', this.toggle.bind(this));
+      this.options = options;
+
+      this.collapsed = true;
     }
 
     toggle() {
       this.$header.find('.chevron').toggleClass('d-none');
-      this.$body.toggleClass('visible');
+      const scrollHeight = this.$body.get(0).scrollHeight;
+      const setOverflow = () => this.$body.css('overflow', scrollHeight > this.options.maxHeight ? 'auto' : 'hidden');
+
+      if (!this.collapsed) {
+        this.$body.animate(
+          { height: 0 },
+          {
+            duration: 300,
+            queue: false,
+            start: setOverflow,
+            complete: setOverflow,
+          },
+        );
+        this.collapsed = true;
+      } else {
+        this.$body.animate(
+          {
+            height: scrollHeight,
+            maxHeight: Math.min(this.options.maxHeight, scrollHeight),
+          },
+          {
+            duration: 300,
+            queue: false,
+            start: setOverflow,
+            complete: setOverflow,
+          },
+        );
+        this.collapsed = false;
+      }
     }
   }
   window.SellixCollapsableComponent = CollapsableComponent;
